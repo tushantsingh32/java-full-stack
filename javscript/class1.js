e23e0e6199087144dffe3a817384a0e2637609aa
Tushant Singh <tushant12356@gmail.com>
2026-01-06 12:23:28 +0530

    init commit

--- javscript/class1.js
+++ javscript/class1.js
@@ -1,2 +1,29 @@
-let c="ilovecoding";
+let req=prompt("enter the reques");
+let task=[];
+while(true)
+{
+    if(req=="list")
+    {
+        for(element of task)
+        {
+            console.log(element);
+        }
+    }
+    else if(req=="add")
+    {
+        let newTask=prompt("enter the task");
+        task.push(newTask);
+    }
+    else if(req=="delete")
+    {
+        let del=prompt("enter the task to delete");
+        
+            task.splice(index,1);
+        
+    }
+    else{
+        console.log("unknown request");
+    }
 
+    req=prompt("enter the request");
+}
